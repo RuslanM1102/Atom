3fa958633df6c9adc78dacf196d7e8980873bcb8
Ma15eR <spaceyourdead@gmail.com>
2023-06-28 17:32:21 +0300

    added scriptable objects

---
+++ Assets/Scripts/Dialog.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "dialogue", menuName = "Dialogue")]
+public class Dialogue : ScriptableObject
+{
+    [SerializeField] private string _textDialogue;
+
+    public string TextDialogue { get => _textDialogue; }
+
+}
